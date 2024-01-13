5ed976a48d65c776b95cce1efb8c6a76b0aede5d
Mxk01 <secretcoderz@yahoo.com>
2024-01-13 22:49:57 +0200

    Changed search functionality

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
-
-function page() {
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { fetchInvoicesPages } from '@/app/lib/data';
+ 
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  // checks if there's a query  "query" lol,if there's none set the query to 1 
+  const query = searchParams?.query || '';
+  // checks if there's a page query ,if there's none then set the currentPage to 1 
+  const currentPage = Number(searchParams?.page) || 1;
+  // THE WAY WE PASS DATA AS WE USE SSR IS WE FETCH DATA ON BACKEND AND SEND IT AS PROPS TO FRONTEND 
+  // total pages is the total amount of calculated pages E.g if we got 12 items and we have 6 items per page then we got
+  // 2 pages
+  const totalPages = await fetchInvoicesPages(query);
   return (
-    <div>invoices</div>
-  )
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
-
-export default page
